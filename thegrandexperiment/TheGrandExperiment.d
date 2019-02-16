312c438da88ddd2627757fe819ccf0e89df010ed
Gregory John Casamento <greg.casamento@gmail.com>
2019-02-16 02:43:41 -0500

    Update script to install apk

--- thegrandexperiment/TheGrandExperiment.d
+++ thegrandexperiment/TheGrandExperiment.d
@@ -1,43 +1,43 @@
 TheGrandExperiment.o: TheGrandExperiment.c android_native_app_glue.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSString.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSVersionMacros.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSConfig.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSObject.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSObjCRuntime.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GNUstep.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSBlocks.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSString.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSVersionMacros.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSConfig.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSObject.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSObjCRuntime.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GNUstep.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSBlocks.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/blocks_runtime.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSObjCRuntime.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSObjCRuntime.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/objc.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/runtime.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/objc-api.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/encoding.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSZone.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSDate.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/NSObject+GNUstepBase.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSRange.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/NSString+GNUstepBase.h \
-  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/NSMutableString+GNUstepBase.h
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSZone.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSDate.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSObject+GNUstepBase.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSRange.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSString+GNUstepBase.h \
+  /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSMutableString+GNUstepBase.h
 
 android_native_app_glue.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSString.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSString.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSVersionMacros.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSVersionMacros.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSConfig.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSConfig.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSObject.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSObject.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSObjCRuntime.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSObjCRuntime.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GNUstep.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GNUstep.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSBlocks.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSBlocks.h:
 
 /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/blocks_runtime.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/GSObjCRuntime.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSObjCRuntime.h:
 
 /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/objc.h:
 
@@ -47,14 +47,14 @@ android_native_app_glue.h:
 
 /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/objc/encoding.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSZone.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSZone.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSDate.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSDate.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/NSObject+GNUstepBase.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSObject+GNUstepBase.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/Foundation/NSRange.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSRange.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/NSString+GNUstepBase.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSString+GNUstepBase.h:
 
-/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/Local/Library/Headers/GNUstepBase/NSMutableString+GNUstepBase.h:
+/Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSMutableString+GNUstepBase.h:
