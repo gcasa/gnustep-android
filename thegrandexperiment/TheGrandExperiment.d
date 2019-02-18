ef5dc8fa6cae063bec5279f16f79a6ad461539c2
Gregory John Casamento <greg.casamento@gmail.com>
2019-02-18 03:51:14 -0500

    Latest changes to scripts.

--- thegrandexperiment/TheGrandExperiment.d
+++ thegrandexperiment/TheGrandExperiment.d
@@ -1,7 +1,80 @@
-TheGrandExperiment.o: TheGrandExperiment.m android_native_app_glue.h \
+TheGrandExperiment.o: TheGrandExperiment.m \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/jni.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdint.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/wchar_limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/cdefs.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/api-level.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/versioning.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/errno.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/errno.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_errno_inlines.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/EGL/egl.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/EGL/eglplatform.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/KHR/khrplatform.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/GLES/gl.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/GLES/glplatform.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/sensor.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/looper.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/posix_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/stddef.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/pthread_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/math.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/posix_limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/log.h \
+  android_native_app_glue.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/poll.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/poll.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/signal.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/signal_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/signal.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/timespec.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/ucontext.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/user.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_signal_inlines.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/time.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/time.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/time.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/select.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/xlocale.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/pthread.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sched.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/sched.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/configuration.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/asset_manager.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_activity.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/input.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/keycodes.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_window.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/data_space.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/inttypes.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/hardware_buffer.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/rect.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSString.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSVersionMacros.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSConfig.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/param.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/param.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/assert.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/ctype.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/locale.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/setjmp.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdio.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/seek_constants.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/struct_file.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdlib.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/alloca.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/wait.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/wait.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/malloc.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_stdlib_inlines.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/string.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/strcasecmp.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSObject.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSObjCRuntime.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GNUstep.h \
@@ -19,13 +92,157 @@ TheGrandExperiment.o: TheGrandExperiment.m android_native_app_glue.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSString+GNUstepBase.h \
   /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/NSMutableString+GNUstepBase.h
 
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/jni.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdint.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/wchar_limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/cdefs.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/api-level.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/versioning.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/errno.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/errno.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_errno_inlines.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/EGL/egl.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/EGL/eglplatform.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/KHR/khrplatform.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/GLES/gl.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/GLES/glplatform.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/sensor.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/looper.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/posix_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/stddef.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/pthread_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/math.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/posix_limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/log.h:
+
 android_native_app_glue.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/poll.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/poll.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/signal.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/signal_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/signal.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/timespec.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/ucontext.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/user.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_signal_inlines.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/time.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/time.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/time.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/select.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/xlocale.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/pthread.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sched.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/sched.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/configuration.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/asset_manager.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_activity.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/input.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/keycodes.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_window.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/data_space.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/inttypes.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/hardware_buffer.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/rect.h:
 
 /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSString.h:
 
 /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSVersionMacros.h:
 
 /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/GNUstepBase/GSConfig.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/param.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/param.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/assert.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/ctype.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/locale.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/setjmp.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdio.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/seek_constants.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/struct_file.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdlib.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/alloca.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/wait.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/wait.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/malloc.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_stdlib_inlines.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/string.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/strcasecmp.h:
 
 /Users/heron/Development/Algoriddim/gnustep-toolchain/gnustep-android/GNUstep/System/Library/Headers/Foundation/NSObject.h:
 
--- thegrandexperiment/android_native_app_glue.d
+++ thegrandexperiment/android_native_app_glue.d
@@ -1,4 +1,181 @@
 android_native_app_glue.o: android_native_app_glue.c \
-  android_native_app_glue.h
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/jni.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdint.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/wchar_limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/cdefs.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/api-level.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/versioning.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/errno.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/errno.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_errno_inlines.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/string.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/xlocale.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/strcasecmp.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/posix_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/stddef.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/pthread_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/unistd.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/select.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/time.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/signal.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/signal_types.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/posix_limits.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/signal.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/timespec.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/ucontext.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/user.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_signal_inlines.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/fcntl.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/getopt.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/ioctl.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/lockf.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/seek_constants.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/sysconf.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/resource.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/resource.h \
+  android_native_app_glue.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/poll.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/poll.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/time.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/time.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/pthread.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sched.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/sched.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/configuration.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/asset_manager.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/looper.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_activity.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/input.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/keycodes.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_window.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/data_space.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/inttypes.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/hardware_buffer.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/rect.h \
+  /Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/log.h
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/jni.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/stdint.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/wchar_limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/cdefs.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/api-level.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/versioning.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/errno.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/errno.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_errno_inlines.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/string.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/xlocale.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/strcasecmp.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/posix_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/stddef.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/compiler.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/pthread_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/unistd.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/select.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/time.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/signal.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/signal_types.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/posix_limits.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/signal.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/timespec.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/ucontext.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/user.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/legacy_signal_inlines.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/fcntl.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/getopt.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/ioctl.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/lockf.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/seek_constants.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/bits/sysconf.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/resource.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/resource.h:
 
 android_native_app_glue.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/poll.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/poll.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/time.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sys/time.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/pthread.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/sched.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/linux/sched.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/configuration.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/asset_manager.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/looper.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_activity.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/input.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/keycodes.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/native_window.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/data_space.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/inttypes.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/hardware_buffer.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/rect.h:
+
+/Users/heron/Library/Android/sdk/ndk-bundle/sysroot/usr/include/android/log.h:
